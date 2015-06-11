95286ae817aa81812125d967d01a39af11356692
LinguList <mattis.list@uni-marburg.de>
2015-06-11 12:15:20 +0200

    updated concepts, go on cleaning

--- stdb-data/helper-2015-06-02.corrections.py
+++
@@ -1,8 +0,0 @@
-from lingpyd.plugins.lpserver.lexibase import LexiBase
-
-lp = LexiBase('sinotibetan',
-        dbase='sinotibetan',
-        #url = 'http://tsv.lingpy.org/triples/sinotibetan.sqlite3'
-        )
-
-
