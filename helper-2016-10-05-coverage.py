f8380eba4869b13c4a23f5dbebe0109e23643855
LinguList <mattis.list@uni-marburg.de>
2016-10-05 16:03:16 +0200

    update data, add missing word forms

--- helper-2016-10-05-coverage.py
+++ helper-2016-10-05-coverage.py
@@ -3,16 +3,19 @@ from lingpy import *
 
 wl = load_sinotibetan(tsv=True)
 concepts = wl.concepts
-text = '# Missing words in the STDB Database\n'
+btext = '# Missing words in the STDB Database\n'
+texts = []
 for t in wl.taxa:
-    text += '## Missing words for {0}\n'.format(t)
+    text = '## Missing words for {0}\n'.format(t)
     _text = []
     words = wl.get_dict(taxa=t)
     for c in concepts:
         if c not in words:
             _text += [c]
     text += '* {0} concepts are missing\n'.format(len(_text))
-    text += '* '+', '.join(_text)+'\n\n'
+    text += '\n'.join(['* {0}'.format(t) for t in _text])+'\n'
+    if _text:
+        texts += [text]
 with open(stdb_path('stats', 'missing.md'), 'w') as f:
-    f.write(text)
+    f.write(btext+'\n'.join(texts))
 
